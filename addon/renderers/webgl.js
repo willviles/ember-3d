47ea9d05babca8a477ad0ca6a0be00ea9613316a
willviles <will@willviles.com>
2016-12-30 10:37:22 +0000

    Allows for options to be passed to WebGLRenderer

--- addon/renderers/webgl.js
+++ addon/renderers/webgl.js
@@ -1,9 +1,14 @@
+import Ember from 'ember';
 import BaseRendererMixin from './base';
-
 import { WebGLRenderer } from 'three';
+
+const { computed, get } = Ember;
 
 export default BaseRendererMixin.extend({
 
-  renderer: new WebGLRenderer()
+  renderer: computed(function() {
+    const options = get(this, 'options') || {};
+    return new WebGLRenderer(options);
+  })
 
 });
